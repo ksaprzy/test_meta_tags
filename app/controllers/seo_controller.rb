efff5d9600f37ace82e00b6bd9a908200a543d36
kubprzy <kubprzy@gmail.com>
2014-08-24 16:37:50 +0200

    commit 1

---
+++ app/controllers/seo_controller.rb
@@ -0,0 +1,16 @@
+class SeoController < ApplicationController
+  def index
+
+    @key_words = Array.new
+    @key_words<<"word1"
+    @key_words<<"word2"
+    @key_words<<"word3"
+
+
+    #
+    # set_meta_tags :title => 'Member Login',
+    #               :description => 'Member login page.',
+    #               :keywords => 'Site, Login, Members'
+
+  end
+end
